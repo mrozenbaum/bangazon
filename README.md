09a9007b89c69e45022d9404b786271932cc499d
Miriam Rozenbaum <miriamrozenbaum@Miriams-MacBook-Pro.local>
2017-04-18 15:28:54 -0500

    bangazon 2 complete

--- README.md
+++ README.md
@@ -85,3 +85,38 @@ Assign values to the properties of each.
 Use print() to output the name of each of your department instances.
 hr_department = HumanResources(...)
 print(hr_department.name)
+
+Part 2:
+
+Bangazon Orientation - Specializing Derived Classes with Overriding
+
+Instructions
+
+Override
+
+Choose one of the general methods that you created in the Department class for overriding. For example, the meet() method which handles the logic of how teammates in that department gather for a meeting.
+Override that method in all of your derived classes, giving each a more specialized implementation. For example, you could output a different meeting place for each department.
+Override, but use parent
+
+Now make a method on Department named get_budget(). It will set, and return, the base budget that each department gets each year. You pick what that number is.
+Override that method in each of the derived classes.
+Make sure you invoke the parent class' overridden method with the super keyword (e.g. super().get_budget()). That will set the base budget.
+Now add, or subtract, from that base budget inside the derived class' override method to adjust that specific department's budget.
+class Department(object):
+  """Parent class for all departments
+
+  Methods: __init__, get_name, get_supervisor, meet
+  """
+
+  def meet():
+    print("Everyone meet in {}'s office".format(self.supervisor))
+class Development(object):
+  """Software development department
+
+  Methods: __init__, get_name, get_supervisor, meet
+  """
+
+  def meet():
+    """This overrides the base meet() method completely"""
+    
+    print("Everyone meet in the server room")
--- bangazon.py
+++ bangazon.py
@@ -1,17 +1,36 @@
+import random
+
 class Department(object):
     """Parent class for all departments"""
 
-    def __init__(self, name, supervisor):
+    def __init__(self, name, supervisor, employee_count, location):
         self.name = name
         self.supervisor = supervisor
+        self.employee_count = employee_count
+        self.location = location
 
+        self.budget = 55,000
         self.employees = set()
+    
+    def get_budget(self):
+        self.budget = 500000
+        return self.budget
+        print(self.budget)
 
-    def get_name(self):
+    def get_department_name(self):
         return self.name
 
     def get_supervisor(self):
         return self.supervisor
+
+    def get_location(self):
+        return self.location
+
+    def get_employee_count(self):
+        return self.employee_count
+
+    def get_employees(self):
+        return self.employees               
 
 
 class HumanResources(Department):
@@ -19,11 +38,14 @@ class HumanResources(Department):
 
     Methods: __init__, add_policy, get_policy, etc.
     """
-    def __init__(self, name, supervisor):
-        super().__init__(name, supervisor)
+    def __init__(self, name, supervisor, employee_count, location):
+        super().__init__
+        # (name, supervisor, employee_count, location)
         self.name = name
         self.supervisor = supervisor
-
+        self.employee_count = employee_count
+        self.location = location
+        self.budget = super().get_budget() + 5000
         self.policies = set()        
 
     def add_policy(self, policy_name, policy_text):
@@ -32,17 +54,27 @@ class HumanResources(Department):
 
         self.policies.add((policy_name, policy_text))
 
+    def get_budget(self):
+        '''Sets the base budget and adds for the dept'''
+        self.budget = super().get_budget() + 50000
+        print(self.budget)    
+
+    # def meet(self):    
+
 
 class IT(Department):
     ''' Class representing IT department
 
     Methods: __init__ & add_computer 
     '''
-    def __init__(self, name, supervisor):
-        super().__init__(name, supervisor, employee)
+    def __init__(self, name, supervisor, employee_count, location):
+        super().__init__
+        # (name, supervisor, employee_count, location)
         self.name = name
         self.supervisor = supervisor
-
+        self.employee_count = employee_count
+        self.location = location
+        self.budget = super().get_budget() - 8000
         self.computer_dict = dict()
 
     def add_computer(self, computer_id, employee_id):
@@ -56,16 +88,49 @@ class IT(Department):
         self.employee_id = employee_id
         self.computer_dict[computer_id] = employee_id
 
+    def get_budget(self):
+        '''Sets the base budget and adds for the dept'''
+        self.budget = super().get_budget() + 40000
+        print(self.budget)    
+
 
 class Marketing(Department):
     ''' Class representing Marketing department
 
     Methods: __init__ & add_sale 
     '''
-    def __init__(self, name, supervisor):
-        super().__init__(name, supervisor)
+    def __init__(self, name, supervisor, employee_count, location):
+        super().__init__
+        # (name, supervisor, employee_count, location)
         self.name = name
         self.supervisor = supervisor
+        self.employee_count = employee_count
+        self.location = location
+
+        self.budget = super().get_budget() - 10000
+
+    def get_budget(self):
+        '''Sets the base budget and adds for the dept'''
+        self.budget = super().get_budget() + 1000000
+        print(self.budget)    
+
+
 
 
 if __name__ == '__main__':
+
+   hr_department = HumanResources('Human Resources', 'Kat Von D', 'California', 5)
+   marketing_department = Marketing('Marketing', 'Beyonce', 'Texas', 10)
+   it_department = IT('Information Technology', 'George Cloony', 'California', 15)
+
+   print(hr_department.name)
+   print(marketing_department.name)
+   print(it_department.name)
+
+   hr_department.add_policy('Abuse Free Zone', 'Employees can not display or engage in any verbal, physical, emotional, or any other kid of  abuse or violence.')
+   print(hr_department.policy_name)
+   print(hr_department.policy_text)
+
+   print('hr_department budget=', hr_department.budget)
+   print('marketing_department budget =', marketing_department.budget)
+   print('it_department budget =', it_department.budget)
