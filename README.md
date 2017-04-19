cd429d40e04485e57ee57a51c3f296f4f401d609
Miriam Rozenbaum <miriamrozenbaum@Miriams-MacBook-Pro.local>
2017-04-19 11:33:31 -0500

    completed bangazon 5

--- README.md
+++ README.md
@@ -165,3 +165,36 @@ eat(food="sandwich") - Will output that the employee ate that specific food at t
 eat(companions=[Sam, Dean, Alice]) - Will select a random restaurant name from a list of strings, print to console that the employee is at that restaurant, and also output the first name of each employee in the specified list.
 eat("pizza", [Sam, Dean, Alice]) - Will select a random restaurant name from a list of strings, print to console that the employee at that restaurant, and ordered the specified food, with the first name of the teammates specified in the list.
 Note: Notice that this signature doesn't require that the parameters to be named
+
+Part 5:
+
+Bangazon Orientation - Aggregation
+
+Instructions
+
+For this exercise, you need to modify the Department class so that Employees can be aggregated into them. In its constructor, initialize an empty set named self.employees.
+
+Create three new methods.
+
+add_employee(self, employee) - Add an employee to the set. The employee parameter accepts an existing instance of an employee.
+remove_employee(self, employee) - Removes an employee from the set. The employee parameter accepts an existing instance of an employee.
+get_employees(self) - Returns the set of employees.
+Write a module that creates an instance of each of your Departments that you have defined. Then create two or three Employee instances for each Department, and add them to the appropriate Department instance.
+
+Once you have defined all of your Employees and Departments, write logic to output the name of each Department, and the first/last name of each employee it contains to the command line.
+
+Example CLI Output
+
+Department: Sales
+   Andri Alexandrou
+   Wayne Hutchinson
+   Sephora Rodriguez
+   Matt Qualls
+   Jen Solima
+
+Department: Technology
+   Caitlin Stein
+   Ryan Tanay
+   Jessawynn Parker
+   Damon Romano
+Once you've completed this basic output, modify it to display any other properties that might be applied to either the Department or Employee that you added from the previous exercises (e.g. handicap, employment status, etc.)
--- bangazon.py
+++ bangazon.py
@@ -3,10 +3,9 @@ import random
 class Department():
     """Parent class for all departments"""
 
-    def __init__(self, name, supervisor, location, employee_count):
-        self.name = name
-        self.supervisor = supervisor
-        self.employee_count = employee_count
+    def __init__(self, name, supervisor, location):
+        self.__name = name
+        self.__supervisor = supervisor
         self.location = location
         self.budget = 550000
         self.employees = set()
@@ -14,12 +13,10 @@ class Department():
     def get_department_name(self):
         return self.name
 
-
     def get_budget(self):
         self.budget = 500000
         return self.budget
         print(self.budget)
-
 
     def get_supervisor(self):
         return self.supervisor
@@ -29,21 +26,22 @@ class Department():
 
     def add_employee(self, employee):
         employee_status = ''
-        try:
-            employee_status = ' hours per pay period: ' + str(employee.hours_per_pay_period)
-        except(AttributeError, NameError):
-            pass
+        self.employees.add(employee)
+        # try:
+        #     employee_status = ' hours per pay period: ' + str(employee.hours_per_pay_period)
+        # except(AttributeError, NameError):
+        #     pass
 
-        try:
-            if employee.access_card == True:
-                employee_status += ' has ACCESS CARD '
-        except(AttributeError, NameError):
-            pass
+        # try:
+        #     if employee.access_card == True:
+        #         employee_status += ' has ACCESS CARD '
+        # except(AttributeError, NameError):
+        #     pass
 
-        try:
-            employee__name = employee.first_name + ' ' + employee.last_name + employee_status + self.employees.add(employee_name)
-        except(AttributeError, NameError):
-                print('Add an Employee')               
+        # try:
+        #     employee__name = employee.first_name + ' ' + employee.last_name + employee_status + self.employees.add(employee_name)
+        # except(AttributeError, NameError):
+        #         print('Add an Employee')               
 
 
     def remove_employee(self, employee):
@@ -54,13 +52,21 @@ class Department():
         except KeyError:
                 print("Employee not found")
 
-                            
-    def get_employee_count(self):
-        return self.employee_count
-
 
     def get_employees(self):
-        print("Department: {}\n\t{}".format(self.name, ("\n\t".join(self.employees))))
+
+        employee_string = ""
+        for employee in self.employees:
+            # print("{} {} is a {} employee.".format(self.first_name, self.last_name, self.employment_type))
+            employee_string +=  "{} {} is a {} employee.\n".format(employee.first_name, employee.last_name, employee.employment_type)
+
+        return employee_string
+
+
+
+# family_string = {v['name'] +' is my '+ k +' and is '+str(v['age'])+' years old.' for(k,v) in my_family.items()}
+# print(family_string)
+
 
 
     @property
@@ -89,7 +95,79 @@ class Department():
         if len(val) > 4:
             self.__supervisor = val
         else:
-            raise ValueError('Please enter a supervisor name longer than 4 characters')                        
+            raise ValueError('Please enter a supervisor name longer than 4 characters')
+
+class Human_Resources(Department):
+    """Class for representing Human Resources department
+
+    Methods: __init__, add_policy, get_policy, etc.
+    """
+
+    def __init__(self, name, supervisor, location):
+        super().__init__(name, supervisor, location)        
+        self.budget = super().get_budget() + 5000
+        self.policies = set()
+
+
+    def add_policy(self, policy_name, policy_text):
+        self.policy_name = policy_name
+        self.policy_text =policy_text
+
+        self.policies.add((policy_name, policy_text))
+
+    def get_budget(self):
+        '''Sets the base budget and adds for the dept'''
+        self.budget = super().get_budget() + 50000
+        print(self.budget)
+
+class IT(Department):
+    ''' Class representing IT department
+
+    Methods: __init__ & add_computer 
+
+    '''
+    def __init__(self, name, supervisor, location):
+        super().__init__(name, supervisor, location)
+        # self.name = name
+        # self.supervisor = supervisor
+        # self.location = location
+        self.budget = super().get_budget() - 8000
+        self.computer_dict = dict()
+
+    def add_computer(self, computer_id, employee_id = 'Not Registered'):
+        ''' Adds a new computer to the computer_dict as a 'key'. If no employeeId is entered it will be assigned Not Registered.
+
+        Arguments:
+        computer_id - (string)
+        employee_id - (string)
+        '''
+        self.computer_id = computer_id
+        self.employee_id = employee_id
+        self.computer_dict[computer_id] = employee_id
+
+    def get_budget(self):
+        '''Sets the base budget and adds for the dept'''
+        self.budget = super().get_budget() + 40000
+        print(self.budget)    
+
+
+class Marketing(Department):
+    ''' Class representing Marketing department
+
+    Methods: __init__ & add_sale 
+    '''
+    def __init__(self, name, supervisor, location):
+        super().__init__(name, supervisor, location)
+        self.budget = super().get_budget() - 10000
+
+
+    def get_budget(self):
+        '''Sets the base budget and adds for the dept'''
+        self.budget = super().get_budget() + 1000000
+        print(self.budget)            
+
+
+
 
 class Employee():
     '''Class representing an employee
@@ -125,22 +203,20 @@ class Full_Time():
     ''' Class representing full-time employee'''
     def __init__(self):
         self.hour_per_week = 40
-
+        self.employment_type = 'Full Time'
 
 
 class Part_Time():
     ''' Class representing part-time employee '''
     def __init__(self):
         self.hour_per_week = 25
-
-
+        self.employment_type = 'Part Time'
 
 
 class Access_Card():
     def __init__(self):
         self.access_card = True
-
-
+        # self.has_access_card = "True"
 
 
 class Human_Resources_Employee(Employee, Full_Time):
@@ -149,7 +225,8 @@ class Human_Resources_Employee(Employee, Full_Time):
 
     def __init__(self, first_name, last_name):
         super().__init__(first_name, last_name)
-        Full_Time.__init__(self)                                                
+        Full_Time.__init__(self)
+                                                    
 
 class IT_Employee(Employee, Part_Time, Access_Card):
 
@@ -167,120 +244,54 @@ class Marketing_Employee(Employee, Full_Time, Access_Card):
     def __init__(self, first_name, last_name):
         super().__init__(first_name, last_name)
         Full_Time.__init__(self)
-
-
-class Human_Resources(Department):
-    """Class for representing Human Resources department
-
-    Methods: __init__, add_policy, get_policy, etc.
-    """
-
-    def __init__(self, name, supervisor, employee_count, location):
-        super().__init__
-        # (name, supervisor, employee_count, location)
-        self.name = name
-        self.supervisor = supervisor
-        self.employee_count = employee_count
-        self.location = location
-        self.budget = super().get_budget() + 5000
-        self.policies = set()        
-
-    def add_policy(self, policy_name, policy_text):
-        self.policy_name = policy_name
-        self.policy_text =policy_text
-
-        self.policies.add((policy_name, policy_text))
-
-    def get_budget(self):
-        '''Sets the base budget and adds for the dept'''
-        self.budget = super().get_budget() + 50000
-        print(self.budget)    
-
-    # def meet(self):    
-
-
-class IT(Department):
-    ''' Class representing IT department
-
-    Methods: __init__ & add_computer 
-
-    '''
-    def __init__(self, name, supervisor, employee_count, location):
-        super().__init__
-        # (name, supervisor, employee_count, location)
-        self.name = name
-        self.supervisor = supervisor
-        self.employee_count = employee_count
-        self.location = location
-        self.budget = super().get_budget() - 8000
-        self.computer_dict = dict()
-
-    def add_computer(self, computer_id, employee_id):
-        ''' Adds a new computer to the computer_dict as a 'key'. If no employeeId is entered it will be assigned Not Registered.
-
-        Arguments:
-        computer_id - (string)
-        employee_id - (string)
-        '''
-        self.computer_id = computer_id
-        self.employee_id = employee_id
-        self.computer_dict[computer_id] = employee_id
-
-    def get_budget(self):
-        '''Sets the base budget and adds for the dept'''
-        self.budget = super().get_budget() + 40000
-        print(self.budget)    
-
-
-class Marketing(Department):
-    ''' Class representing Marketing department
-
-    Methods: __init__ & add_sale 
-    '''
-    def __init__(self, name, supervisor, employee_count, location):
-        super().__init__
-        # (name, supervisor, employee_count, location)
-        self.name = name
-        self.supervisor = supervisor
-        self.employee_count = employee_count
-        self.location = location
-
-        self.budget = super().get_budget() - 10000
-
-    def get_budget(self):
-        '''Sets the base budget and adds for the dept'''
-        self.budget = super().get_budget() + 1000000
-        print(self.budget)    
-
+        Access_Card.__init__(self)
 
 
 
 if __name__ == '__main__':
 
-   hr_department = Human_Resources('Human Resources', 'Kat Von D', 'California', 5)
-   marketing_department = Marketing('Marketing', 'Beyonce', 'Texas', 10)
-   it_department = IT('Information Technology', 'George Cloony', 'California', 15)
-   # Prints the name of each of department instances (exercise 1)
-   print(hr_department.name)
-   print(marketing_department.name)
-   print(it_department.name)
+   hr_department = Human_Resources('Human Resources', 'Kat Von D', 'California')
+   marketing_department = Marketing('Marketing', 'Beyonce', 'Texas')
+   it_department = IT('Information Technology', 'George Cloony', 'California')
 
-   hr_department.add_policy('Abuse Free Zone', 'Employees can not display or engage in any verbal, physical, emotional, or any other kid of  abuse or violence.')
-   print(hr_department.policy_name)
-   print(hr_department.policy_text)
+   # Prints the name of each of department instances (exercise 1)
+   # print(hr_department.name)
+   # print(marketing_department.name)
+   # print(it_department.name)
+
+   hr_department.add_policy('Abuse Free Zone', 'Employees can not display or engage in any verbal, physical, emotional, or any other kind of abuse or violence.')
+   # print(hr_department.policy_name)
+   # print(hr_department.policy_text)
    # #Prints adjusted budget for each department (exercise 2)
-   print('hr_department budget=', hr_department.budget)
-   print('marketing_department budget =', marketing_department.budget)
-   print('it_department budget =', it_department.budget)
+   # print('hr_department budget=', hr_department.budget)
+   # print('marketing_department budget =', marketing_department.budget)
+   # print('it_department budget =', it_department.budget)
 
    #Prints statements from method overloadng (exercise 3)
    miriam = Employee("Miriam", "Rozenbaum")
-   print(miriam)
-   print('--------this is miriam.eat():', miriam.eat())
+   # print(miriam)
+   # print('--------this is miriam.eat():', miriam.eat())
    miriam.eat('banana', ['Ruthie', 'Dara', 'Casey'])
    miriam.eat(companions =["Ruthie", "Dara", "Casey"])
    miriam.eat(food="sandwich", companions= ["Casey", "Ruthie", "Dara"])
 
+   matan = Human_Resources_Employee('Matan', 'Ohad')
+   charlie = Human_Resources_Employee('Charlie', 'Pug')
+   ruthie = IT_Employee('Ruthie', 'Wilkerson')
+   miriam = Marketing_Employee('Miriam', 'Rozenbaum')
 
+   hr_department.add_employee(matan)
+   hr_department.add_employee(charlie)
+   it_department.add_employee(ruthie)
+   marketing_department.add_employee(miriam)
+
+   # print('Department:', hr_department.name)
+   # print('Department:', it_department.name)
+   # print('Department:', marketing_department.name)
+   print(hr_department.get_employees())
+   print(it_department.get_employees())
+   print(marketing_department.get_employees())
+   hr_department.get_employees()
+   # hr_department.get_budget()
 
 
